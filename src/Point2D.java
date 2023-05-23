c6febb281001bf5ee1a71f3211bf072cd2da4c2f
aliATYPON <134340952+aliATYPON@users.noreply.github.com>
2023-05-23 14:47:58 +0300

    Update Point2D.java
    ali modify the branch

--- src/Point2D.java
+++ src/Point2D.java
@@ -20,7 +20,8 @@ public class Point2D {
     }
 
     public double distanceTo(Point2D other) {
-        return Math.sqrt(Math.pow(x - other.getX(), 2) + Math.pow(y - other.getY(), 2));
+               return System.out.println("ali here");
+
     }
 }
 
